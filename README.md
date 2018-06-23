b2206c5d801de07dfe423a2da53ed8f89c914d7d
Joel-David Wong <joeldavid.wong@gmail.com>
2018-06-23 18:35:44 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # node-red-contrib-beacon-scanner
+
+Node to allow Node-RED scan for the following beacons:
+- iBeacons
+- EddyStones
+- Estimote
+
+Dependencies 
+-------
+- Node.js 6 >
+- Noble
+- node-beacon-scanner
+
+Installation
+-------
+
+Run the following command in your Node-RED directory
+	
+    npm i noble
+    npm i node-beacon-scanner
+
+Usage
+-----
+
+### Beacon Scanner
+
+Messages emitted are stated in [Futomi's repo](https://github.com/futomi/node-beacon-scanner#beaconscanneradvertisement-object).
---
+++ beacon-scanner.js
@@ -0,0 +1,36 @@
+module.exports = function(RED) {
+    "use strict";
+    const BeaconScanner = require('node-beacon-scanner');
+    const scanner = new BeaconScanner();
+
+    function Scanner(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+
+        scanner.onadvertisement = (ad) => {
+            node.send({
+                payload: ad
+            });
+        };
+
+        scanner.startScan().then(() => {
+            node.status({
+                fill: "green",
+                shape: "dot",
+                text: "Scanning Started"
+            });
+        }).catch((error) => {
+            node.status({
+                fill: "red",
+                shape: "dot",
+                text: error
+            });
+        });
+
+        node.on('close', function(done) {
+            scanner.stopScan();
+            done();
+        });
+    }
+    RED.nodes.registerType("BeaconScanner", Scanner);
+};
